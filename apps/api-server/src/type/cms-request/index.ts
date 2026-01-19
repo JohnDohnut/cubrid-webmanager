ace9f52aa1b4fc984b43e2af94d371b6541dfdd8
JohnDohnut <51821505+JohnDohnut@users.noreply.github.com>
2026-01-19 18:03:22 +0900

    [TOOLS-4743] Backend Devlopment - Job Automation API (#3)
    * autoated backup URI modified
    
    * comment refined
    
    * set(modifier) for automated backup added

--- apps/api-server/src/type/cms-request/index.ts
+++ apps/api-server/src/type/cms-request/index.ts
@@ -23,6 +23,7 @@ export * from './get-all-sys-param-cms-request';
 export * from './set-sys-param-cms-request';
 export * from './get-admin-log-info-cms-request';
 export * from './add-backup-info-cms-request';
+export * from './set-backup-info-cms-request';
 export * from './get-backup-info-cms-request';
 export * from './set-auto-exec-query-cms-request';
 export * from './get-auto-exec-query-cms-request';
